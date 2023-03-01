8f2c2e6274f9f12164087c8f2bf63370bd13fb67
Manab Biswas <77844663+manab-kb@users.noreply.github.com>
2023-03-01 15:01:53 +0000

    Client class implementation

--- Network/client.py
+++ Network/client.py
@@ -1,11 +1,15 @@
 from socket import *
 
-# Specifying loopback address as hostname alongsides accepting port number
-hostname = "127.0.0.1"
-portnum = int(input("Port Number: "))
-
-# Creating a TCP socket and using it to connect to the server socket
-with socket(AF_INET, SOCK_STREAM) as skt:
-    skt.connect((hostname, portnum))
-    skt.sendall(b"Hello, world")
-    data = skt.recv(1024)
+# Class containing member functions and variables for implementation of a client
+class client:
+    def __init__(self, hostname, portnum):
+        # Specifying loopback address as hostname alongsides accepting port number
+        self.host = hostname
+        self.port = portnum
+    
+    def connect(self):
+        # Creating a TCP socket and using it to connect to the server socket
+        with socket(AF_INET, SOCK_STREAM) as skt:
+            skt.connect((self.host, self.port))
+            skt.sendall(b"@log: client connected to server")
+            data = skt.recv(1024)
